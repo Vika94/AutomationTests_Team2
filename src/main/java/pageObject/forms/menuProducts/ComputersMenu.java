ab306708e44d548b8bdfd41b7d665fd1a167c450
AzarovaLena <azarova.l375@gmail.com>
2021-11-09 15:34:45 +0300

    edit test

--- src/main/java/pageObject/forms/menuProducts/ComputersMenu.java
+++ src/main/java/pageObject/forms/menuProducts/ComputersMenu.java
@@ -4,9 +4,10 @@ import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
+import static org.openqa.selenium.By.xpath;
 
 public enum ComputersMenu {
-    NOTEBOOKS($(By.xpath("//div[contains(text(),'Ноутбуки')]")));
+    NOTEBOOKS($(xpath("//div[contains(text(),'Ноутбуки')]")));
 
     protected SelenideElement element;
 
